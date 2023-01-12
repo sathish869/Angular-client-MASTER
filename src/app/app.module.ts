cbdc7965b8206d7a1fa3d99c399f884f272ee06e
SathishKumar Sellamuthu <SathishKumar.Sellamuthu@flatironssolutions.com>
2023-01-12 21:52:08 +0530

    Assign-1 completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {MatListModule} from '@angular/material/list';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { HttpClientModule } from '@angular/common/http';
+import { ProgressSpinnerComponent } from './list-of-user/progress-spinner/progress-spinner.component';
+import { HeaderComponent } from './header/header.component';
 import { AppComponent } from './app.component';
+import { ListOfUserComponent } from "./list-of-user/list-of-user.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent,
+        HeaderComponent,
+        ListOfUserComponent,
+        ProgressSpinnerComponent],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserAnimationsModule,
+        BrowserModule,
+        FormsModule,
+        MatProgressSpinnerModule,
+        HttpClientModule,
+        MatListModule,
+        ReactiveFormsModule,
+    ]    
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/list-of-user/progress-spinner/progress-spinner.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+/**
+ * @title Basic progress-spinner
+ */
+@Component({
+  selector: 'progress-spinner',
+  templateUrl: './progress-spinner.component.html',
+  styleUrls: ['./progress-spinner.component.css']
+})
+export class ProgressSpinnerComponent {
+  
+}
